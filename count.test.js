2d0fe7b10d051f7e2892610bcd6a1f6563fbde05
stucodegreen <m.kuznecovas@gmail.com>
2019-10-07 13:10:07 +0100

    update

--- count.test.js
+++ count.test.js
@@ -1,48 +1,23 @@
 const countComission = require('./count');
+let outputData = "";
+global.console = {
+  log: jest.fn(),
+  info: jest.fn(),
+  error: jest.fn()
+}
 
-test('Counted', () => {
-	expect(countComission.count([{
-		user_id: 1,
-		user_type: 'natural',
-		type: 'cash_in',
-		operation: [ 200 ],
-		date: [ '2016-01-05' ]
-	}])).toEqual(0.06)
+
+describe('Counted', () => {
+	it('Counted',() => {
+		countComission.count([{
+			user_id: 1,
+			user_type: 'natural',
+			type: 'cash_in',
+			operation: [ 200 ],
+			date: [ '2016-01-05' ]
+		}])
+		expect(global.console.log).toHaveBeenCalledWith(0.06)
+	})
+
 })
-// test('Counted', () => {
-// 	expect(countComission.count([{
-// 		user_id: 2,
-//     user_type: 'juridical',
-//     type: 'cash_out',
-//     operation: [ 300 ],
-//     date: [ '2016-01-06' ]
-//   }])).toEqual(0.9)
-// })
-// test('Counted', () => {
-// 	expect(countComission.count([{
-//     user_id: 1,
-//     user_type: 'natural',
-//     type: 'cash_out',
-//     operation: [ 30000, 1000, 100, 100, 300 ],
-//     date: [
-//       '2016-01-06',
-//       '2016-01-07',
-//       '2016-01-07',
-//       '2016-01-10',
-//       '2016-02-15'
-//     ]
-//   }])).toEqual(87)
-// })
-// test('Counted', () => {
-// 	expect(countComission.count([{
-//     user_id: 1,
-//     user_type: 'natural',
-//     type: 'cash_out',
-//     operation: [  1000 ],
-//     date: [
-//       '2016-01-07'
-//     ]
-//   }])).toEqual(0)
-// })
-
 
