19f4859d2a63933eca8a15558aa2c1ee818ae759
stucodegreen <m.kuznecovas@gmail.com>
2019-10-07 11:19:17 +0100

    update

--- count.js
+++ count.js
@@ -26,9 +26,9 @@ function countCom(num, per) {
   let output = (num / 100) * per;
   return roundUp(output, 1);
 }
-
+function sortData(data){
 // this will check for same ids and will create new array
-const src = JSON.parse(JSON.stringify(inputContent));
+const src = JSON.parse(JSON.stringify(data));
 const sortedData = src.reduce(
   (acc, { user_id, user_type, type, operation, date }) => {
     const existing = acc.find(i => i.user_id === user_id && i.type === type);
@@ -49,55 +49,64 @@ const sortedData = src.reduce(
   },
   []
 );
-console.log(sortedData);
-
-const countComission = (result) => {
-  for (let i = 0; i < result.length; i++) {
-    // console.log(result[i])
-    let totalsum = 0;
-    for (let si = 0; si < result[i].date.length; si++) {
-      if (result[i].type == 'cash_out' && result[i].user_type === 'natural') {
-        if (
-          moment(result[i].date[si]).isSame(result[i].date[si + 1], 'week') ||
-          moment(result[i].date[si]).isSame(result[i].date[si], 'week')
-          // moment(result[i].date).isSame(result[i+1].date)
-        ) {
-          let comissionCost = naturalContent.percents;
-          currentSum = result[i].operation[si];
-          if (currentSum >= 1000 && totalsum < 1000) {
-            currentSum = currentSum - 1000;
-          } else if (currentSum < 1000 && totalsum < 1000) {
-            comissionCost = 0;
-          }
-
-          let comission = countCom(currentSum, comissionCost);
-
-          totalsum += result[i].operation[si] + result[i].operation[si + 1];
-          console.log(comission);
-          // console.log(`${comission} ${result[i].date[si]} ${currentSum} ${totalsum}` )
-          if (
-            !moment(result[i].date[si]).isSame(result[i].date[si + 1], 'week')
-          ) {
-            totalsum = 0;
-          }
-        }
-      }
-    }
-
-    if (result[i].user_type === 'juridical' && result[i].type === 'cash_out') {
-      let comission = countCom(result[i].operation, legalContent.percents);
-      comission < legalContent.min.amount ? (comission = 0.5) : comission;
-      console.log(comission);
-      // console.log(`${comission} ${result[i].operation}` )
-    }
-    if (result[i].type === 'cash_in') {
-      let comission = countCom(result[i].operation, cashInContent.percents);
-      comission > cashInContent.max.amount ? (comission = 5) : comission;
-      console.log(comission);
-      // console.log(`${comission} ` )
-    }
-  }
+return sortedData;
 }
-countComission(sortedData);
+
+console.log(sortData(inputContent));
+
+const countComission =  {
+	count: (result) => {
+		result = sortData(result)
+		for (let i = 0; i < result.length; i++) {
+			// console.log(result[i])
+			let totalsum = 0;
+			for (let si = 0; si < result[i].date.length; si++) {
+				if (result[i].type == 'cash_out' && result[i].user_type === 'natural') {
+					if (
+						moment(result[i].date[si]).isSame(result[i].date[si + 1], 'week') ||
+						moment(result[i].date[si]).isSame(result[i].date[si], 'week')
+						// moment(result[i].date).isSame(result[i+1].date)
+					) {
+						let comissionCost = naturalContent.percents;
+						currentSum = result[i].operation[si];
+						if (currentSum >= 1000 && totalsum < 1000) {
+							currentSum = currentSum - 1000;
+						} else if (currentSum < 1000 && totalsum < 1000) {
+							comissionCost = 0;
+						}
+	
+						let comission = countCom(currentSum, comissionCost);
+	
+						totalsum += result[i].operation[si] + result[i].operation[si + 1];
+						// console.log(comission);
+						// console.log(`${comission} ${result[i].date[si]} ${currentSum} ${totalsum}` )
+						if (
+							!moment(result[i].date[si]).isSame(result[i].date[si + 1], 'week')
+						) {
+							totalsum = 0;
+						}
+						return comission;
+					}
+				}
+			}
+	
+			if (result[i].user_type === 'juridical' && result[i].type === 'cash_out') {
+				let comission = countCom(result[i].operation, legalContent.percents);
+				comission < legalContent.min.amount ? (comission = 0.5) : comission;
+				console.log(comission);
+				return comission;
+				// console.log(`${comission} ${result[i].operation}` )
+			}
+			if (result[i].type === 'cash_in') {
+				let comission = countCom(result[i].operation, cashInContent.percents);
+				comission > cashInContent.max.amount ? (comission = 5) : comission;
+				console.log(comission);
+				return comission;
+				// console.log(`${comission} ` )
+			}
+		}
+	}
+}
+// countComission.count(inputContent);
 
 module.exports = countComission;
--- count.test.js
+++ count.test.js
@@ -1,11 +1,77 @@
-const {countComission} = require('./count')
-
-test('Tested', () => {
-  expect(countComission([{
-		user_id: 1,
-		user_type: 'natural',
-		type: 'cash_in',
-		operation: [ 200 ],
-		date: [ '2016-01-05' ]
-	},])).toBe(0.06)
+const countComission = require('./count');
+let answers = [
+  {
+    user_id: 1,
+    user_type: 'natural',
+    type: 'cash_in',
+    operation: [ 200 ],
+    date: [ '2016-01-05' ]
+  },
+  {
+    user_id: 2,
+    user_type: 'juridical',
+    type: 'cash_out',
+    operation: [ 300 ],
+    date: [ '2016-01-06' ]
+  },
+  {
+    user_id: 1,
+    user_type: 'natural',
+    type: 'cash_out',
+    operation: [ 30000, 1000, 100, 100, 300 ],
+    date: [
+      '2016-01-06',
+      '2016-01-07',
+      '2016-01-07',
+      '2016-01-10',
+      '2016-02-15'
+    ]
+  },
+  {
+    user_id: 2,
+    user_type: 'juridical',
+    type: 'cash_in',
+    operation: [ 1000000 ],
+    date: [ '2016-01-10' ]
+  },
+  {
+    user_id: 3,
+    user_type: 'natural',
+    type: 'cash_out',
+    operation: [ 1000 ],
+    date: [ '2016-01-10' ]
+  }
+]
+// test('Counted', () => {
+// 	expect(countComission.count([{
+// 		user_id: 1,
+// 		user_type: 'natural',
+// 		type: 'cash_in',
+// 		operation: [ 200 ],
+// 		date: [ '2016-01-05' ]
+// 	},])).toBe(0.06)
+// })
+// test('Counted', () => {
+// 	expect(countComission.count([{
+//     user_id: 2,
+//     user_type: 'juridical',
+//     type: 'cash_out',
+//     operation: [ 300 ],
+//     date: [ '2016-01-06' ]
+//   },])).toBe(0.9)
+// })
+// test('Counted', () => {
+// 	expect(countComission.count([{
+// 		user_id: 1,
+// 		user_type: 'natural',
+// 		type: 'cash_out',
+// 		operation: [ 30000 ],
+// 		date: [
+// 			'2016-01-06',
+// 		]
+// 	},])).toBe(87)
+// })
+test('Counted', () => {
+	expect(countComission.count([  { "date": "2016-01-05", "user_id": 1, "user_type": "natural", "type": "cash_in", "operation": { "amount": 200.00, "currency": "EUR" } }])).toBe(0)
 })
+
